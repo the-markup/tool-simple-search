7ec7901f29cdf836d1a05015c16490cf51cb9a4b
maddy <maddy@themarkup.org>
2020-09-29 14:43:52 -0400

    Inline version

--- extension/simplesearch.js
+++ extension/simplesearch.js
@@ -1,58 +1,45 @@
 // Make close button
-const closeButton = document.createElement('button');
+var clear = document.getElementsByClassName('clear-button')[0];
+const closeButton = clear.cloneNode(true);
+console.log(closeButton);
+closeButton.removeAttribute('jsaction');
+closeButton.removeAttribute('jscontroller');
+closeButton.removeAttribute('jsname');
+closeButton.removeAttribute('data-ved');
+closeButton.setAttribute('aria-label', ' Close');
 closeButton.id = '_ss_closebtn';
 closeButton.onclick = "document.getElementById('_ss_header').style.display = 'none';";
-closeButton.textContent = 'close';
 
 // Make header
 const controls = document.createElement('div');
 controls.id = '_ss_header';
-controls.appendChild(closeButton)
+controls.appendChild(closeButton);
 controls.appendChild(document.createTextNode('Simple Search'));
-//controls.style.position = "fixed";
-//controls.style.top = '0';
-//controls.style.width = '100%';
-//controls.style.height = '2em';
-//controls.style.background = "#FF0";
-//controls.style.border = '1px solid red';
 
 // Make results box
 const results = document.createElement('div');
 results.id = '_ss_results';
-//results.style.padding = "1em";
-//results.style.border = "1px solid blue";
-//results.style.paddingTop = '2em';
-//results.style.maxWidth = '800px';
-//results.style.maxHeight = '540px';
-//results.style.overflow = "auto";
 
+// Make footer
 const explanation = document.createElement('div');
-explanation.id = "_ss_explanation";
-explanation.innerText = "Get back to the good old days where you just had ten blue links. This is by The Markup, a very cool non-profit news site. Like this? DONATE."
-results.appendChild(explanation);
+explanation.id = '_ss_footer';
+explanation.innerHTML = "<h3>Did you know? 41% of the first page of Google search results is taken up by Google products.</h3> That's why we, at The Markup, built this browser extension. You can learn more about this and our investigation into Google Search results on themarkup.org";
 
-// Make modal box
+// Make box
 const viewbox = document.createElement('div');
 viewbox.id = '_ss_box';
 viewbox.appendChild(controls);
 viewbox.appendChild(results);
+viewbox.appendChild(explanation);
 
 const blur = document.createElement('blur');
-
-//viewbox.style.zIndex = "2147483647";
-//viewbox.style.position = 'fixed';
-//viewbox.style.border = "5px solid red";
-//viewbox.style.background = "#FFF";
-//viewbox.style.maxWidth = '800px';
-//viewbox.style.maxHeight = '600px';
-//viewbox.style.overflow = "hidden";
 
 function showPopup() {
 	var y = document.getElementsByClassName("rc");
 	if (y.length > 0) {
-		document.scrollingElement.scrollTop = 0;
+		//document.scrollingElement.scrollTop = 0;
 
-		document.body.insertBefore(viewbox, document.body.firstChild);
+		document.getElementById('center_col').insertBefore(viewbox, document.getElementById('center_col').firstChild);
 
 		for (var i = 0; i < y.length; i++) {
   			var ttl = y[i].getElementsByClassName('r')[0].getElementsByTagName('a')[0];
@@ -67,19 +54,23 @@ function showPopup() {
   			
 
   			if (cont != "" && desc != "") {
-  				results.innerHTML += "<div class='ss_result' style='border:1px solid green; margin-bottom:.5em;'>" + 
-  				"<sub>" + cite + "</sub><br/>" + title + "<br/>" + desc + "<br/></div>";
+  				results.innerHTML += "<div class='_ss_result'>" + 
+  				"<sub>" + cite + "</sub><br/>" + title + desc + "<br/></div>";
   			}
   		} 
 	
 		// Position modal window
-		viewbox.style.left = "50%";
-		viewbox.style.top = "50%";
-		viewbox.style.transform = "translate(-50%, -50%)";
+		//viewbox.style.left = "50%";
+		//viewbox.style.top = "50%";
+		//viewbox.style.transform = "translate(-50%, -50%)";
 
 		// blur the results
-		document.getElementById('rcnt').style.filter = 'blur(5px)';
-		document.body.style.overflow = 'hidden';
+		//document.getElementById('rcnt').style.display = 'none';
+		document.getElementById('rhs').style.display = 'none';
+		document.getElementById('res').style.display = 'none';
+		document.getElementById('bres').style.display = 'none';
+		document.getElementById('brs').style.display = 'none';
+		//document.body.style.overflow = 'hidden';
 
 		// add listener for control buttons
 		window.addEventListener("click", function(e) {
@@ -87,8 +78,10 @@ function showPopup() {
 				return;
 			}
     		document.getElementById(viewbox.id).style.display = 'none';
-    		document.getElementById('rcnt').style.filter = '';
-    		document.body.style.overflow = '';
+    		document.getElementById('rhs').style.display = '';
+			document.getElementById('res').style.display = '';
+			document.getElementById('bres').style.display = '';
+			document.getElementById('brs').style.display = '';
 		});
 	}
 }
