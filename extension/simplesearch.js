f763198d2a3335b6bd82a3441860a9b75b7306c7
Sam Morris <sam@themarkup.org>
2020-09-29 17:16:23 -0400

    re-write results script

--- extension/simplesearch.js
+++ extension/simplesearch.js
@@ -7,10 +7,8 @@ markupLabel.innerHTML = 'Results cleaned up by <a class="ss-label__link" href="h
 const closeButton = document.createElement('div');
 closeButton.classList.add('ss-close');
 closeButton.innerHTML = '<p class="ss-close__label">View Original Results</p><svg version="1.1" class="ss-close__button" xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 1024 1024" xml:space="preserve"><path d="M91.6 91.6l840.8 840.8M932.4 91.6L91.6 932.4"/></svg>';
-closeButton.addEventListener('click', function(e) {
-	console.log('clicked');
-
-	// Figure out how Maddy swapped out simple results for old ones
+closeButton.addEventListener('click', function() {
+	document.querySelector('body').classList.remove('ss--has-results');
 });
 
 // Make header
@@ -25,56 +23,41 @@ results.id = '_ss_results';
 
 // Make footer
 const explanation = document.createElement('div');
-explanation.id = '_ss_footer';
-explanation.innerHTML = "<h3>Did you know? 41% of the first page of Google search results is taken up by Google products.</h3> That's why we, at The Markup, built this browser extension. You can learn more about this and our investigation into Google Search results on themarkup.org";
+explanation.classList.add('ss-footer');
+explanation.innerHTML = '<h3 class="ss-footer__title">Did you know? 41% of the first page of Google search results is taken up by Google products.</h3><p class="ss-footer__description">That\'s why we, at The Markup, built this browser extension. You can learn more about this and our investigation into Google Search results on <a class="ss-footer__link" target="_blank" href="https://themarkup.org">themarkup.org</a></p>';
 
 // Make box
 const viewbox = document.createElement('div');
-viewbox.id = '_ss_box';
+viewbox.classList.add('ss-box');
 viewbox.appendChild(controls);
 viewbox.appendChild(results);
 viewbox.appendChild(explanation);
 
-const blur = document.createElement('blur');
-
 function showPopup() {
-	console.log('show popup');
-	var y = document.getElementsByClassName("rc");
-	if (y.length > 0) {
-		//document.scrollingElement.scrollTop = 0;
+	// Get Page Results
+	const googleResults = document.querySelectorAll('.rc');
 
-		document.getElementById('center_col').insertBefore(viewbox, document.getElementById('center_col').firstChild);
+	if (googleResults.length > 0) {
+		// Add placeholder for results if we've found results
+		document.querySelector('#center_col').prepend(viewbox);
 
-		for (var i = 0; i < y.length; i++) {
-  			var ttl = y[i].getElementsByClassName('r')[0].getElementsByTagName('a')[0];
-  			var link = ttl.href;
-  			var rel = ttl.rel;
-  			var cont = ttl.getElementsByTagName('h3')[0].innerText;
+		// Populate new results with those clean results
+		googleResults.forEach(function(result, i) {
+			console.log(result);
+			const linkEl = result.querySelector('a');
+			const url = linkEl.href;
+			const rel = linkEl.rel;
+			const title = result.querySelector('h3').innerHTML;
+			const desc = result.querySelector('div > div > span > span:last-of-type');
+			const cite = result.querySelector('cite');
 
-  			var title = "<a href='" + link + "' rel='" + rel + "'><h3>" + cont + "</h3></a>";
-  			var desc = y[i].getElementsByClassName('st')[0].innerHTML;
+			if (desc && cite) {
+				results.innerHTML += '<div class="ss-result"><h4 class="ss-result__cite">' + cite.innerText + '</h4><a href="' + url + '" rel="' + rel + '" class="ss-result__link">' + title + '</a><p class="ss-result__description">' + desc.innerHTML + '</p></div>';
+			}
+		});
 
-  			var cite = y[i].getElementsByTagName('cite')[0].innerText;
-  			
-
-  			if (cont != "" && desc != "") {
-  				results.innerHTML += "<div class='_ss_result'>" + 
-  				"<sub>" + cite + "</sub><br/>" + title + desc + "<br/></div>";
-  			}
-  		} 
-	
-		// Position modal window
-		//viewbox.style.left = "50%";
-		//viewbox.style.top = "50%";
-		//viewbox.style.transform = "translate(-50%, -50%)";
-
-		// blur the results
-		//document.getElementById('rcnt').style.display = 'none';
-		document.getElementById('rhs').style.display = 'none';
-		document.getElementById('res').style.display = 'none';
-		document.getElementById('bres').style.display = 'none';
-		document.getElementById('brs').style.display = 'none';
-		//document.body.style.overflow = 'hidden';
+		// Set a class to make it all visible
+		document.querySelector('body').classList.add('ss--has-results');
 	}
 }
 
