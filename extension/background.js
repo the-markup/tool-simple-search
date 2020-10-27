41ade28a5c12f7cd59335f836cef3b89b705c8a3
maddy <maddy@themarkup.org>
2020-10-26 21:09:10 -0400

    mostly done, bing injection bug TK

--- extension/background.js
+++ extension/background.js
@@ -1,10 +1,12 @@
+
+// when initially installed, set the toggle to true
 chrome.runtime.onInstalled.addListener(function (details) {
 	chrome.storage.sync.set({"toggle": true});
 });
 
+// when someone clicks, change the toggle setting
 chrome.browserAction.onClicked.addListener(function(tab) {
 	// Get the current setting
-
 	var key = "toggle";
 	chrome.storage.sync.get([key], function(result) {
 		var toggle = result[key];
@@ -12,11 +14,18 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 		// flip the setting
 		toggle = !toggle;
 
+		// inform the page
+
+		// update the icon
+		updateIcon(toggle);
+
 		//save the new setting
 		chrome.storage.sync.set({'toggle': toggle});
 	});
 });
 
+// listen for toggle setting change
+/*
 chrome.storage.onChanged.addListener(function(changes, namespace) {
     for (key in changes) {
     	if (key === 'toggle') {
@@ -25,20 +34,16 @@ chrome.storage.onChanged.addListener(function(changes, namespace) {
     	}
     }
 });
+*/
 
-function updateIcon() {
-	var key = "toggle";
-	chrome.storage.sync.get([key], function(result) {
-		var toggle = result[key];
-
-		if (toggle) {
-			chrome.browserAction.setIcon({
-				"path": 'icons/on.png'
-			});
-		} else {
-			chrome.browserAction.setIcon({
-				"path": 'icons/off.png'
-			});
-		}
-	});
+function updateIcon(toggle) {
+	if (toggle) {
+		chrome.browserAction.setIcon({
+			"path": 'icons/on.png'
+		});
+	} else {
+		chrome.browserAction.setIcon({
+			"path": 'icons/off.png'
+		});
+	}
 }
